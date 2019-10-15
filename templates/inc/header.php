d585918a1527a9236f6e22130164fcf4589d4d60
Nino Obanor <nino.obanor@perficient.com>
2019-10-15 10:43:28 -0500

    First Commit

---
+++ templates/inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JobLister</title>
+    <link rel = "stylesheet" href="css/journal.min.css">
+    <!-- <link rel = "stylesheet" href="css/bootstrap.min.css">   -->
+    <link rel = "stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header clearfix">
+            <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation"><a href="index.php">Home </a></li>
+                <li role="presentation"><a href="create.php">Create Listing</a></li>
+            </ul>
+            </nav>
+            <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
+        </div>
+    <?php displayMessage();?>
